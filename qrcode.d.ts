8f6838d6f78ba8268ed1592e3329b2c2ff5e13c3
Zaw Linn Htet <zaw.linn.htet03@gmail.com>
2025-10-14 19:27:25 +0630

    Added qrcode

---
+++ qrcode.d.ts
@@ -0,0 +1,6 @@
+declare module 'qrcode' {
+  // Minimal typings to satisfy TypeScript during build
+  // You can replace this with `@types/qrcode` if you prefer.
+  const QRCode: any;
+  export default QRCode;
+}
